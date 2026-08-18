143dc089c3e72b8d0ac45dfdd4ca81073efe0fd1
Bradley Dice <bdice@bradleydice.com>
2026-08-18 11:00:23 -0500

    Fix devcontainer cache version updates (#8485)
    Update devcontainer cache image tags missed by the 26.10 version bump.
    
    Teach the version update script to update these tags in future releases.
    
    xref: rapidsai/build-planning#316
    
    Authors:
      - Bradley Dice (https://github.com/bdice)
    
    Approvers:
      - James Lamb (https://github.com/jameslamb)
    
    URL: https://github.com/NVIDIA/cuml/pull/8485

--- ci/release/update-version.sh
+++ ci/release/update-version.sh
@@ -144,6 +144,7 @@ done
 # .devcontainer files
 find .devcontainer/ -type f -name devcontainer.json -print0 | while IFS= read -r -d '' filename; do
     sed_runner "s@rapidsai/devcontainers:[0-9.]*@rapidsai/devcontainers:${NEXT_SHORT_TAG}@g" "${filename}"
+    sed_runner "s@ghcr.io/rapidsai/cuml/devcontainer:[0-9.]*@ghcr.io/rapidsai/cuml/devcontainer:${NEXT_SHORT_TAG}@g" "${filename}"
     sed_runner "s@rapidsai/devcontainers/features/ucx:[0-9.]*@rapidsai/devcontainers/features/ucx:${NEXT_SHORT_TAG_PEP440}@" "${filename}"
     sed_runner "s@rapidsai/devcontainers/features/cuda:[0-9.]*@rapidsai/devcontainers/features/cuda:${NEXT_SHORT_TAG_PEP440}@" "${filename}"
     sed_runner "s@rapidsai/devcontainers/features/rapids-build-utils:[0-9.]*@rapidsai/devcontainers/features/rapids-build-utils:${NEXT_SHORT_TAG_PEP440}@" "${filename}"
